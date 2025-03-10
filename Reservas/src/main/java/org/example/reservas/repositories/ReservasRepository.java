440ad9f53b924bc7923bd541cd62c92216abf874
Antonio Castillo Jiménez <134335720+antoniocastillo-08@users.noreply.github.com>
2025-03-10 03:59:58 +0100

    Delete Reservas directory

--- Reservas/src/main/java/org/example/reservas/repositories/ReservasRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.reservas.repositories;
-
-import org.example.reservas.models.Reserva;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReservasRepository extends JpaRepository<Reserva, Long> {
-}
